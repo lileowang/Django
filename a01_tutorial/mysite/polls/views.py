162057d5c2a057a4cb1aa410cac7ad5e428bc69a
lileowang <wangli20@hotmail.com>
2020-08-24 11:00:43 -0700

    Added more views.

--- a01_tutorial/mysite/polls/views.py
+++ a01_tutorial/mysite/polls/views.py
@@ -3,5 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("polls index page.")
+    return HttpResponse('polls index page.')
+
+
+def detail(request, question_id):
+    response = 'question %s.'
+    return HttpResponse(response % question_id)
+
+
+def results(request, question_id):
+    response = 'results of question %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = 'voting on question %s.'
+    return HttpResponse(response % question_id)
