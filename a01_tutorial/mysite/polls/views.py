d4be1a5044b5347bb447400003e08901ee651cf3
lileowang <wangli20@hotmail.com>
2020-08-25 00:03:49 -0700

    Added detail view and replaced hard-coded url.

--- a01_tutorial/mysite/polls/views.py
+++ a01_tutorial/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 from .models import Question
@@ -13,8 +13,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    response = 'question %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
