fea95cda620981f27c014be46d18299acefade95
lileowang <wangli20@hotmail.com>
2020-08-24 22:47:50 -0700

    Implemented index views.

--- a01_tutorial/mysite/polls/views.py
+++ a01_tutorial/mysite/polls/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-from .models import Question
-
-# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
     # get last 2 records
     latest_question_list = Question.objects.order_by('-pub_date')[:2]
-    output = '<br>'.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
