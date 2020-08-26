a8b183d5872f8502bca37c0847cf766c8dd7779d
lileowang <wangli20@hotmail.com>
2020-08-26 00:50:10 -0700

    Added practice project.

---
+++ a02_practice/mysite/polls/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:2]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': 'you did not select a choice'
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
