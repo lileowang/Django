f3c6a01faaf857b877f9ada6f3aef4328b0e8cbf
lileowang <wangli20@hotmail.com>
2020-08-28 00:18:37 -0700

    Moved javascript into separate file.

---
+++ a02_practice/mysite/polls/static/polls/script.js
@@ -0,0 +1,27 @@
+// alert('hello');
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#submit').disabled = true;
+
+    document.querySelector('#task').onkeyup = () => {
+        // console.log('keyup');
+        if (document.querySelector('#task').value.length > 0) {
+            document.querySelector('#submit').disabled = false;
+        } else {
+            document.querySelector('#submit').disabled = true;
+        }
+    };
+
+    document.querySelector('form').onsubmit = () => {
+        let task = document.querySelector('#task').value;
+        console.log(task);
+
+        const li = document.createElement('li');
+        li.innerHTML = task;
+        document.querySelector('#tasks').append(li);
+
+        document.querySelector('#task').value = '';
+        document.querySelector('#submit').disabled = true;
+
+        return false;
+    };
+});
